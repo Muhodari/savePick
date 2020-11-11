ab3b96bcbc60f018b6345899ce25d1036e8ea75a
Muhodari <sagemuho@gmail.com>
2020-11-11 01:40:35 -0800

    commit mastwe hh

---
+++ src/app/auth/auth.interceptor.ts
@@ -0,0 +1,8 @@
+import { HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http'
+
+export class AuthInterceptor implements HttpInterceptor{
+    intercept(req:HttpRequest<any>,next:HttpHandler){
+
+        return next.handle(req);
+    }
+}
